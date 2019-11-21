77d7219b2a39008a8f80729343e7284f4a5f654e
YakobusNobel <nobelemailtugas@gmail.com>
2019-11-21 12:33:09 +0700

    pesan kalian

---
+++ precache-manifest.db4f9c1446aad9c919f8b66d03241d3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6b9b810d72a64ba065",
+    "url": "/css/chunk-vendors.a70f79b7.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e9db3bf123a4e43fa16008fa2ec3c33e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f9cb9ec987bb56c78db",
+    "url": "/js/app.c8fc1166.js"
+  },
+  {
+    "revision": "2f6b9b810d72a64ba065",
+    "url": "/js/chunk-vendors.e0e25785.js"
+  },
+  {
+    "revision": "a042a27ac443df22a63459a9b70983db",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
